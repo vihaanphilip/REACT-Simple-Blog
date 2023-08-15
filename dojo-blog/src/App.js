923f093df2e32c97be076074adddd58408389dc0
Vihaan Philip <viha0001@student.monash.edu>
2023-08-15 12:47:46 +0800

    Create Navbar

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 100;
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <h1> { title } </h1>
+        <p>Liked { likes } times</p>
+        <p>{ "hello, ninjas" }</p>
+
+
+
+
       </div>
     </div>
   );
