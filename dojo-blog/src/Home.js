03910e79e2c02547b62b101e975ced574080fe91
Vihaan Philip <viha0001@student.monash.edu>
2023-09-12 00:23:49 +0800

    Lean about useStates

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+
 const Home = () => {
+
+    // let name = 'mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+    // const handleClick = (e) => {
+    //     console.log('hello, ninjas!', e);
+    // }
+
+    // const handleClickAgain = (name, e) => {
+    //     console.log('hello ' + name, e.target);
+    // }
+
+    const handleClick = () => {
+        setName('luigi');
+        setAge(30);
+    }
+ 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old </p>
+            <button onClick={handleClick}>Click me</button>
+            {/* <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button> */}
         </div>
      );
 }
  
-export default Home;
+export default Home; 
